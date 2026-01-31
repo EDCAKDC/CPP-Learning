2c5496cfc739d52c86f67629ac680781848859a6
EDCAKDC <georgehealer233@gmail.com>
2026-01-30 22:59:28 -0500

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -58,6 +58,7 @@ int main()
 }
 */
 
+/*
 int main()
 {
     char a;
@@ -72,4 +73,71 @@ int main()
 
     return 0;
 }
+*/
 
+/******************************************************
+ * Date: 2026-01-30
+ * Topic: C++ function return type & boolalpha
+ * Notes:
+ *  - Learned that function return type is fixed
+ *  - double -> int will truncate decimal
+ *  - std::boolalpha prints true/false instead of 1/0
+ ******************************************************/
+
+/*
+ double add(double x, double y)
+{
+    return x + y;
+}
+
+int main() {
+    double a = 5.7, b = 9.5;
+    std::cout << add(a, b) << '\n';
+}
+*/
+
+/*
+double prod(double x, double y)
+{
+    return x * y;
+}
+
+int main() {
+
+    double a, b;
+
+    std::cout << "Input a" << '\n';
+    std::cin >> a;
+
+    std::cout << "Input b" << '\n';
+    std::cin >> b;
+
+    std::cout << "Result: " << prod(a, b) << '\n';
+    return 0;
+}
+*/
+
+double prod(double x, double y)
+{
+    return x * y;
+}
+
+int main() {
+
+    double a, b;
+
+    std::cout << "Input a" << '\n';
+    if (!(std::cin >> a)){
+        std::cerr << "Invalid input for a\n";
+        return 1;
+    }
+
+    std::cout << "Input b" << '\n';
+    if (!(std::cin >> b)){
+        std::cerr << "Invaild input for b\n";
+        return 1;
+    }
+
+    std::cout << "Result: " << prod(a, b) << '\n';
+    return 0;
+}
